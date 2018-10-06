0bdeb997f9cc1cf94b381798629a31856547a839
bae <s@seanbae.net>
2018-10-06 13:41:37 -0700

    Add response header

--- src/api.js
+++ src/api.js
@@ -19,6 +19,7 @@ let api = expressPromiseRouter()
 
 /** Get the full application state. */
 api.get('/state', (req, res) => {
+  res.set('Access-Control-Allow-Headers', 'Content-Type');
   res.json({
     state: getState(),
     spotPriceHistory: getSpotInstancePriceHistory(),
